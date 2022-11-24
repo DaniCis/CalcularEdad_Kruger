d08503020d967592638b8849d18b2f7b7420e01a
DaniCis <danicisneros2@hotmail.com>
2022-11-23 23:40:50 -0500

    funcion calcular corregida

--- src/components/Edad.jsx
+++ src/components/Edad.jsx
@@ -3,11 +3,12 @@ import Card from "./Card";
 
 const Edad = () =>{
     const [fechaNacimiento,setFecha] = useState();
-    const [day,setDay] = useState();
-    const [month,setMonth] = useState();
-    const [year,setYear] = useState();
+    const [day,setDay] = useState(0);
+    const [month,setMonth] = useState(0);
+    const [year,setYear] = useState(0);
 
     const obtenerFecha = (e) =>{
+        //console.log(e.target.valueAsDate.toLocaleDateString())
         const fecha = document.querySelector('input[type="date"]')
         setFecha(fecha.value)
     }
@@ -16,29 +17,39 @@ const Edad = () =>{
         const actualYear = new Date().getFullYear()
         const actualmonth = new Date().getMonth() + 1
         const actualDay = new Date().getUTCDate()
-        const[year, month, day] = fechaNacimiento.split("-")
-        
-        //no cumple todavia
-        if(month > actualmonth){
-            setYear(actualYear - year - 1)
-            if(day > actualDay){
-                setMonth((actualmonth -month)+11)
-                setDay((actualDay - day)+31)
-            }
-            else{
-                setMonth((actualmonth - month)+12)
-                setDay(actualDay - day)
-            }
-        }else{
-            if(month == actualmonth && day > actualDay){
-                setYear(actualYear - year - 1)
-                setDay((actualDay - day)+30)
-                setMonth((actualmonth - month)+11)
-            }   
-            else{
-                setMonth(actualmonth -month)
-                setYear(actualYear - year)
-                setDay(actualDay - day)
+        const[bornYear, bornMonth, bornDay] = fechaNacimiento.split("-")
+
+        setYear(actualYear -  bornYear)
+        setMonth(actualmonth - bornMonth)
+        setDay(actualDay - bornDay)
+
+        //no cumple años todavia
+        if(bornMonth > actualmonth || (bornMonth == actualmonth && bornDay > actualDay) )
+            setYear(actualYear - bornYear - 1);
+        //meses
+        if(bornMonth > actualmonth)
+            setMonth(actualmonth - bornMonth + 12)
+        else if(bornMonth == actualmonth && bornDay > actualDay){
+            if(bornMonth - actualmonth == 0)//no cumple aun
+                setMonth(11)
+            else 
+                setMonth(0)
+        }
+        //dias
+        if(actualDay - bornDay < 0 ){
+            if(actualmonth - bornMonth -1 < 0)
+                setMonth(actualmonth - bornMonth -1 + 12)    
+            else
+                setMonth(actualmonth - bornMonth -1)
+            switch(bornMonth){
+                case '01': case '03': case '05': case '07':case '08':case '10':case '12':  
+                    setDay(actualDay - bornDay + 31)
+                    break;
+                case '02': 
+                    setDay(actualDay - bornDay + 28)
+                    break;
+                default:
+                    setDay(actualDay - bornDay + 30)
             }
         }
     }
@@ -46,8 +57,8 @@ const Edad = () =>{
     return (
         <div className="contenedor">
             <div className="contenedorFecha">
-                <input type='date' onChange={obtenerFecha} pattern="\d{4}-\d{2}-\d{2}"></input>
-                <button onClick={calcularEdad}>Calcular</button>
+                <input className="input-fecha" type='date' onChange={obtenerFecha} pattern="\d{4}-\d{2}-\d{2}"></input>
+                <button className="btn-calcular" onClick={calcularEdad}>Calcular</button>
             </div>
             <div className="contenedorEdad">
                 <Card valor={year} unidad='años' />
